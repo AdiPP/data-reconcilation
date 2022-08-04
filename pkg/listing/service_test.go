9b5f17d5741ba66bd8d66a8358acdc20cccb52b1
Adi Putra <adiputrapermana@gmail.com>
2022-08-04 22:37:00 +0700

    initialize test case

---
+++ pkg/listing/service_test.go
@@ -0,0 +1,87 @@
+package listing
+
+import (
+	"testing"
+	"time"
+
+	"github.com/AdiPP/reconciliation/pkg/storage/memory"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindAllProxies(t *testing.T) {
+	mr := newMockStorage()
+	s := NewService(mr)
+
+	_, err := s.FindAllProxies()
+
+	assert.Nil(t, err)
+}
+
+type mockStorage struct {
+	proxies []Proxy
+	sources []Source
+}
+
+func (m *mockStorage) FindAllProxies() ([]memory.Proxy, error) {
+	proxies := []memory.Proxy{}
+
+	for _, pp := range m.proxies {
+		p := memory.Proxy{
+			ID:     pp.ID,
+			Amount: pp.Amount,
+			Desc:   pp.Desc,
+			Date:   pp.Date,
+		}
+		proxies = append(proxies, p)
+	}
+
+	return proxies, nil
+}
+
+func (m *mockStorage) FindSourceByID(ID string) (memory.Source, error) {
+	var source memory.Source
+
+	for _, ss := range m.sources {
+		if ss.ID == ID {
+			return memory.Source{
+				ID:     ss.ID,
+				Amount: ss.Amount,
+				Desc:   ss.Desc,
+				Date:   ss.Date,
+			}, nil
+		}
+	}
+
+	return source, memory.ErrSourceNotFound
+}
+
+func newMockStorage() *mockStorage {
+	return &mockStorage{
+		[]Proxy{
+			{
+				ID:     "zoUr",
+				Amount: 24,
+				Desc:   "A",
+				Date:   getDate("2021-06-30"),
+			},
+		},
+		[]Source{
+			{
+				ID:     "zoUr",
+				Amount: 24,
+				Desc:   "A",
+				Date:   getDate("2021-06-30"),
+			},
+		},
+	}
+}
+
+func getDate(dateString string) time.Time {
+	date, err := time.Parse("2006-01-02", dateString)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return date
+}
